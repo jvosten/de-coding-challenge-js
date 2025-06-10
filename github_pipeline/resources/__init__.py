9118d9a2c290e0f658d77c81d7cd59bb212994bf
jvosten <21955156+jvosten@users.noreply.github.com>
2025-06-10 23:32:08 +0200

    docs: further elaborate on iomanager

--- github_pipeline/resources/__init__.py
+++ github_pipeline/resources/__init__.py
@@ -5,6 +5,12 @@ from .gh_resource import GitHubAPIResource
 from .s3_json_io_manager import S3JsonIOManager
 from .s3_md_io_manager import S3MdIOManager
 
+# Resource that gives access to S3.
+# S3Resource provides a get_client method that returns a boto3 client for interacting with S3.
+# For our purposes, we will use the S3Resource to pass just the credentials to our IOManagers.
+# This works because S3Resource is a subclass of ResourceWithS3Configuration, which is what
+# we use in our IOManagers to configure s3fs
+
 s3_resource = S3Resource(
     aws_access_key_id=EnvVar('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=EnvVar('AWS_SECRET_ACCESS_KEY'),
